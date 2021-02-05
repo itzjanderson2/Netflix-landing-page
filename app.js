a33b87062320ae1de10ffb3a46e81de64a723ec9
itzjanderson2 <64199226+itzjanderson2@users.noreply.github.com>
2021-02-04 22:22:33 -0600

    Create app.js

---
+++ app.js
@@ -0,0 +1,36 @@
+const tabItems = document.querySelector('.table-items');
+const tabContentItems =  document.querySelector('.table-content-items');
+ 
+// Select tab content item
+function selectItem (e){
+    // Remove all show and border classes
+    removeBorder();
+    removeShow();
+    // Add Border to current tab item
+    this.classList.add('table-border');
+    // Grab content item from DOM
+    const tabContentItem = document.querySelector('#${this.id}-content');
+    // Add show class
+    tabContentItem.classList.add('show');
+}
+ 
+// Remove bottom borders from all tab items
+function removeBorder() {
+    tabItems.forEach(item => {
+        item.classList.remove('table-border');
+    });
+}
+ 
+// Remove show class from all content items
+function removeShow() {
+    tabContentItems.forEach(item => {
+        item.classList.remove('show');
+    });
+}
+ 
+// Listen for tab item click
+tabItems.forEach(item => {
+    item.addEventListener('click', selectItem);
+});
+ 
+
